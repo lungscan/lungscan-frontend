866c7786599a06be33feb279596fc4ea41b9c235
souorichard <rodrigues.lpta@gmail.com>
2025-06-10 20:58:27 -0300

    chore: update footer and layout structure

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,12 +5,14 @@ import { PropsWithChildren } from 'react'
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
-    <div className="px-4 py-5 w-full max-w-4xl min-h-screen mx-auto space-y-5">
-      <Header />
+    <div className="min-h-screen px-4 py-5 flex flex-col gap-8">
+      <div className="w-full max-w-4xl flex-1 mx-auto space-y-5">
+        <Header />
 
-      <Separator />
+        <Separator />
 
-      {children}
+        {children}
+      </div>
 
       <Footer />
     </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,26 +2,18 @@ import { Separator } from './ui/separator'
 
 export function Footer() {
   return (
-    <div className=" pt-54">
-      <Separator />
-      <footer className="flex items-center gap-3 justify-between pt-8 pb-3">
-        <div className="flex items-centergap-6 justify-between gap-6">
-          <h1 className="text-l font-bold">Other projects</h1>
+    <footer className="w-full max-w-4xl mx-auto pt-4 flex items-center gap-5 border-t">
+      <h1 className="text-l font-bold">Other project(s)</h1>
 
-          <Separator orientation="vertical" className="!h-5" />
-          <p className="text-justify text-muted-foreground">
-            <span className="text-foreground font-medium text-gray-400">
-              <a
-                href="https://github.com/Buscavan"
-                className="hover:underline"
-                target="_blank"
-              >
-                Buscavan
-              </a>
-            </span>
-          </p>
-        </div>
-      </footer>
-    </div>
+      <Separator orientation="vertical" className="!h-5" />
+
+      <a
+        href="https://github.com/Buscavan"
+        className="text-sm text-muted-foreground font-medium hover:underline"
+        target="_blank"
+      >
+        Buscavan
+      </a>
+    </footer>
   )
 }
